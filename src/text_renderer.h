01c55a0b4be6618318279c212d29e0f600c2d528
Sven <svenpra2013@gmail.com>
2019-04-14 01:44:15 +0200

    some fixes in text renderer

--- src/text_renderer.h
+++ src/text_renderer.h
@@ -1,41 +1,58 @@
+#include <stdlib.h>
+
 #include <SDL2/SDL_ttf.h>
 
-static const SDL_Color SDL_WHITE_COLOR = { 255, 255, 255 };
+const SDL_Color SDL_WHITE_COLOR = { 255, 255, 255 };
 
 typedef struct {
-    char* current_text;
+    char* text;
+
+    SDL_Renderer* sdl_renderer;
 
     SDL_Color font_color;
     TTF_Font* font;
     SDL_Texture* texture;
 } text_renderer_t;
 
-text_renderer_t* renderer_create(const char* path_to_font, int font_size = 16, SDL_Color font_color = SDL_WHITE_COLOR) {
-    text_renderer_t renderer = malloc(sizeof(text_renderer_t));
+void text_renderer_set_text(text_renderer_t* renderer, char* text) {
+     printf("XD\n");
+    renderer->text = text;
+     printf("SD\n");
 
-    text_renderer_set_text(renderer, "NULL");
-
-    return renderer;
+    SDL_Surface* surface = TTF_RenderText_Solid(renderer->font, text, renderer->font_color);
+     printf("wd\n");
+    renderer->texture = SDL_CreateTextureFromSurface(renderer->sdl_renderer, surface);
+     printf("DewKW\n");
+    SDL_FreeSurface(surface);
+     printf("dwa\n");
 }
 
-void text_renderer_update_font(text_renderer_t* renderer, const char* path_to_font, int font_size = 16) {
+void text_renderer_update_font(text_renderer_t* renderer, char* path_to_font, int font_size) {
     renderer->font = TTF_OpenFont(path_to_font, font_size);
+    if (!renderer->font)
+        printf("Error loading font!\n");
     text_renderer_set_text(renderer, renderer->text);
 }
 
 void text_renderer_update_font_color(text_renderer_t* renderer, SDL_Color font_color) {
     renderer->font_color = font_color;
-    text_renderer_set_text(renderer->text);
+    text_renderer_set_text(renderer, renderer->text);
 }
 
-void text_renderer_set_text(text_renderer_t* renderer, const char* text) {
-    renderer->text = text;
-
-    SDL_Surface* surface = TTF_RenderText_Solid(renderer->font, text, renderer->font_color);
-    renderer->texture = SDL_CreateTextureFromSurface(sdl_renderer, surface);
-    SDL_FreeSurface(surface);
+void text_renderer_render(text_renderer_t* renderer, SDL_Rect dest) {
+    SDL_RenderCopy(renderer->sdl_renderer, renderer->texture, NULL, &dest);
 }
 
-void text_renderer_render(text_renderer_t* renderer, SDL_Rect dest, SDL_Renderer* sdl_renderer) {
-    SDL_RenderCopy(sdl_renderer, renderer->texture, NULL, &dest);
+text_renderer_t* text_renderer_create(SDL_Renderer* sdl_renderer, char* path_to_font, int font_size, SDL_Color font_color) {
+    text_renderer_t* renderer = malloc(sizeof(text_renderer_t));
+
+    renderer->sdl_renderer = sdl_renderer;
+
+    renderer->text = "NULL";
+    renderer->font_color = SDL_WHITE_COLOR;
+
+    text_renderer_update_font(renderer, path_to_font, font_size);
+    
+    printf("setted text\n");
+    return renderer;
 }
